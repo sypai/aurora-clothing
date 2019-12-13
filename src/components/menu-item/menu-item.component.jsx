e40792ddfe059cb5bb2a2b9ab65ab189982650f4
sypai <7suyashbajpai@gmail.com>
2019-12-13 18:20:57 +0530

    Menu items: Styling added, Hover effect

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -4,13 +4,15 @@ import './menu-item.styles.scss'
 
 const MenuItem = ({ title, img, size }) =>
 (
-    <div 
-        style={{backgroundImage: `url(${img})`}} 
-        className={`${size} menu-item`}>
+    <div className={`${size} menu-item`}>
+
+        <div className='background-image' 
+             style={{backgroundImage: `url(${img})`}} >
+        </div>
 
         <div className='content'>
             
-            <h1 className='title'> {title} </h1>
+            <h1 className='title'> {title.toUpperCase()} </h1>
             <span className='subtitle'> SHOP NOW</span>
         
         </div>
