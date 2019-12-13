8132e19b9800602977d3443b0d61a50f5b6ad390
sypai <7suyashbajpai@gmail.com>
2019-12-13 17:16:36 +0530

    create react-app and added files for homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import HomePage from './components/homepage/homepage.components';
+
 function App() {
   return (
     <div>
-      Hello World!
+      <HomePage />
     </div>
   );
 }
