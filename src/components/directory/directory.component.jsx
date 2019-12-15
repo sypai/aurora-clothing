1197922a8ce3c94f1fa64f8e0a3ea76eff4edb2a
sypai <7suyashbajpai@gmail.com>
2019-12-15 16:11:34 +0530

    routing in react, react-router-dom, updated with some es6 tricks

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -29,18 +29,18 @@ class Directory extends React.Component
                   linkUrl: 'shop/sneakers'
                 },
                 {
-                  title: 'womens',
+                  title: 'women',
                   imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-                  size: 'large',
                   id: 4,
-                  linkUrl: 'shop/womens'
+                  linkUrl: 'shop/women',
+                  size: 'large'
                 },
                 {
-                  title: 'mens',
+                  title: 'men',
                   imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-                  size: 'large',
                   id: 5,
-                  linkUrl: 'shop/mens'
+                  linkUrl: 'shop/men',
+                  size: 'large'
                 }
               ]
         }
@@ -51,8 +51,10 @@ class Directory extends React.Component
         return (
             <div className='directory-menu'>
             {
-                this.state.sections.map(({ title, imageUrl, id, size }) => (
-                    <MenuItem key={id} title={title} img={imageUrl} size={size} />
+                // this.state.sections.map(({ title, imageUrl, id, size, linkUrl }) => (
+                this.state.sections.map(({ id, ...otherSectionProps }) => (
+                    // <MenuItem key={id} title={title} img={imageUrl} size={size} linkUrl={linkUrl} />
+                    <MenuItem key={id} {...otherSectionProps} />
                     )
                 )
             }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
 
+import { withRouter } from 'react-router-dom'
+
 import './menu-item.styles.scss'
 
-const MenuItem = ({ title, img, size }) =>
+
+const MenuItem = ({ title, imageUrl, linkUrl,  size, history, match }) =>
 (
-    <div className={`${size} menu-item`}>
+    <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`) }>
 
         <div className='background-image' 
-             style={{backgroundImage: `url(${img})`}} >
+             style={{backgroundImage: `url(${imageUrl})`}} >
         </div>
 
         <div className='content'>
             
             <h1 className='title'> {title.toUpperCase()} </h1>
-            <span className='subtitle'> SHOP NOW</span>
+            <span className='subtitle'> SHOP NOW </span>
         
         </div>
     
     </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
